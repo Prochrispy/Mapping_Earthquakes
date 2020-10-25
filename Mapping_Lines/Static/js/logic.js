f2af5a34b0f81a8b50764859e534a88ef26b3c2c
Prochrispy <68368541+Prochrispy@users.noreply.github.com>
2020-10-25 12:54:54 -0400

    Multiple Lines done

--- Mapping_Lines/Static/js/logic.js
+++ Mapping_Lines/Static/js/logic.js
@@ -1,7 +1,7 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
-// We create the map object with options
-llet map = L.map('mapid').setView([37.6213, -122.3790], 5); 
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 // Coordinates for each point to be used in the line.
 let line = [
     [33.9416, -118.4085],
