36cb7f0bcac098a43b5da3717a75e8219f81f304
Prochrispy <68368541+Prochrispy@users.noreply.github.com>
2020-10-25 12:51:03 -0400

    multiple lines

--- Mapping_Lines/Static/js/logic.js
+++ Mapping_Lines/Static/js/logic.js
@@ -1,13 +1,18 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // We create the map object with options
-let map = L.map('mapid').setView([40.7, -94.5], 4);
-//  Add a marker to the map for Los Angeles, California.
-L.circle([34.0522, -118.2437], {
-    radius: 300,
-    color: "yellow",
-    fillcolor: '#ffffa1'
- }).addTo(map);
+llet map = L.map('mapid').setView([37.6213, -122.3790], 5); 
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow"
+  }).addTo(map);
 // We create the tile layer that will be the background of our map.
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {    
